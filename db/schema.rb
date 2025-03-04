e3150854718d8c85e63de219552612b927083ca8
Dulcelene Machado <dudutola@gmail.com>
2025-03-04 21:49:14 +0000

    Integrated OmniAuth for GitHub authentication Updated User model to handle GitHub login Ensured fallback values for missing GitHub data (email, nickname, name, avatar, provider, and UID) Modified Devise configuration to support OmniAuth Added github_uid, github_username, github_name, github_avatar_url, and provider fields to the User model Handled user lookup by uid and email to prevent duplicate accounts Implemented a fallback for missing UID using SecureRandom.uuid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_03_162414) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_04_200759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_162414) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_issues_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,8 +31,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_03_162414) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "github_avatar_url"
+    t.string "github_username"
+    t.string "github_name"
+    t.string "github_uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "issues", "users"
 end
