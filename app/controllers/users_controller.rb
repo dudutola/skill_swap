fcde646a859aab8f73d4293c5425a2e1d9c445b0
Dulcelene Machado <dudutola@gmail.com>
2025-03-05 09:26:59 +0000

    Added actions show and profile to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  class UsersController < ApplicationController
+    def show
+      @user = User.find(params[:id])
+    end
+
+    def profile
+      @current_user = current_user
+    end
+  end
 end
