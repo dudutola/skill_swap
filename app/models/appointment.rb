ffb578dca2d142c426fa9878702e611a0be92fe0
Dulcelene Machado <dudutola@gmail.com>
2025-03-06 12:39:22 +0000

    Implemented logic to create a new appointment with tutor and pupil association Added validations to model Appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :tutor, class_name: "User"
   belongs_to :pupil, class_name: "User"
   belongs_to :issue
+
+  validates :issue, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
