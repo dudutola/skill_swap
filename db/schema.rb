b189f2be45afba1bd5e86d05f8efc39b4166f562
Dulcelene Machado <dudutola@gmail.com>
2025-03-07 10:41:11 +0000

    Generated modal tagging and its migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_07_102125) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_07_103544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_07_102125) do
     t.bigint "user_id"
     t.text "available_on"
     t.index ["user_id"], name: "index_issues_on_user_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.bigint "issue_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["issue_id"], name: "index_taggings_on_issue_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -66,4 +75,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_07_102125) do
   add_foreign_key "appointments", "users", column: "pupil_id"
   add_foreign_key "appointments", "users", column: "tutor_id"
   add_foreign_key "issues", "users"
+  add_foreign_key "taggings", "issues"
+  add_foreign_key "taggings", "tags"
 end
