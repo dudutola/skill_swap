8165375f7ff83f8eb0c33535db6cc5d5ee2e6ed6
Dulcelene Machado <dudutola@gmail.com>
2025-03-07 10:44:36 +0000

    Added tagging associations to issue and tag models

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,6 +1,8 @@
 class Issue < ApplicationRecord
   belongs_to :user
   has_many :appointments, dependent: :destroy
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
 
   validates :user, presence: true
   validates :title, presence: true
